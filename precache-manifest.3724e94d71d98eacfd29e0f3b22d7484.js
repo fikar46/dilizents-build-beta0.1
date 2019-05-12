631ad8bb5690c953526f1f87640afb55ae19dc8e
fikar46 <43537558+fikar46@users.noreply.github.com>
2019-05-12 12:03:11 +0700

    dilizent 0.1

---
+++ precache-manifest.3724e94d71d98eacfd29e0f3b22d7484.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0561f0dafa6326c5453c",
+    "url": "/static/js/main.acba93d7.chunk.js"
+  },
+  {
+    "revision": "68e8c333022a1ad8fc1b",
+    "url": "/static/js/2.5923963d.chunk.js"
+  },
+  {
+    "revision": "0561f0dafa6326c5453c",
+    "url": "/static/css/main.92dd6f4f.chunk.css"
+  },
+  {
+    "revision": "df26cb774cf2950865121e3776c760c4",
+    "url": "/index.html"
+  }
+];
